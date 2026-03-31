94934a9b7c5a73620b08f43b513ee65a45a3c585
Phoenix <53673135+likitha4@users.noreply.github.com>
2026-03-31 17:08:44 +0530

    Set default port to 8000 if not specified

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const axios = require("axios");
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 
 app.use(cors({
   origin: ['http://localhost:5173', 'https://lessgo-crypto.onrender.com']
